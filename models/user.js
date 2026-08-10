0ab5abed87012e4db227dfda0ef9ba7efdfb7235
Mrigank <mrigankumaraj@gmail.com>
2026-08-10 22:01:23 +0530

    Registratuion Problem Fixed

--- models/user.js
+++ models/user.js
@@ -3,7 +3,17 @@ const { default: passportLocalMongoose } = require("passport-local-mongoose");
 
 
 
+if (!process.env.MONGO_URI) {
+    console.error("MONGO_URI is not set, using fallback localhost URI");
+}
 mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/resting");
+const db = mongoose.connection;
+db.on("error", (err) => {
+    console.error("MONGO CONNECTION ERROR:", err.message);
+});
+db.once("open", () => {
+    console.log("MongoDB connected");
+});
 
 const userSchema = new mongoose.Schema({
     fullname: {
